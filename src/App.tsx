196b40de708aa027671b3a62cb5aa833161ff3f8
RicardoC23 <ricardomamani403@gmail.com>
2023-04-20 20:47:13 -0500

    estructura del Portafolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
 import { Header } from './Componets/Header/header'
 import { Hero } from './Componets/Hero/Hero'
 import { Footer } from './Componets/Footer/Footer'
 import { About } from './Componets/About-me/About'
 import { Tecnologias } from './Componets//TecnologiasS/Tecnologias'
+import { Portfolio } from './Componets/Portfolio/Portfolio'
 
 function App() {
   
@@ -13,6 +13,7 @@ function App() {
       <Hero/>
       <About/>
       <Tecnologias/>
+      <Portfolio/>
       <Footer/>
     </>
   )
